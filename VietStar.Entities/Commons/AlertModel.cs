ebdff7becb7dc7bc690ad498cb93ced507cd6f7b
quangphat <phat.dinhquang@haravan.com>
2020-09-05 23:51:41 +0700

    alert

---
+++ VietStar.Entities/Commons/AlertModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VietStar.Entities.Commons
+{
+    public class AlertModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ActionName { get; set; }
+        public string ActionHref { get; set; }
+    }
+}
