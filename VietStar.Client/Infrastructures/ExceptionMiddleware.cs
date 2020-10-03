08812d5e6d47ae5126b69bff90f804c0daa4774e
quangphat <phat.dinhquang@haravan.com>
2020-10-03 13:52:44 +0700

    Globl Exception

---
+++ VietStar.Client/Infrastructures/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using VietStar.Client.Models;
+using VietStar.Repository.Interfaces;
+
+namespace VietStar.Client.Infrastructures
+{
+    public static class ExceptionMiddleware
+    {
+        public static void ConfigureexceptionHandler(this IApplicationBuilder app, ILogRepository rpLog)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await rpLog.InsertLog("exception-error", contextFeature.Error.ToString());
+                        await context.Response.WriteAsync(new ExceptionDetail()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error.",
+                            Error = contextFeature.Error.ToString()
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ VietStar.Client/Models/ExceptionDetail.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VietStar.Client.Models
+{
+    public class ExceptionDetail
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
